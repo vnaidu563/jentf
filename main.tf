609291931ab55cfb7563ca56ea6a4cf0fd51580d
vnaidu563 <vnaidu563@gmail.com>
2024-02-14 15:48:24 -0500

    baseterraform

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"  # Update with your desired region
+}
+
+resource "aws_instance" "JenTFInst" {
+  ami           = "ami-0e731c8a588258d0d"  # Update with your AMI ID
+  instance_type = "t2.micro"                # Update with your desired instance type
+  key_name      = "tfkp"                    # Update with your EC2 key pair name
+
+  tags = { Name = "JenTFInst"}
+}
